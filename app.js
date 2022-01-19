7a64cd8f770185adcac5204a2eadcff8baa34cda
Somanath1458 <37767811+Somanath1458@users.noreply.github.com>
2022-01-19 14:01:59 +0530

    add get and post

--- app.js
+++ app.js
@@ -1,116 +1,81 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const ejs = require('ejs')
-const mongoose = require('mongoose')
+const https = require('https')
+const { google } = require('googleapis')
 
 const app = express()
+app.use(express.json())
 
-app.set('view engine', 'ejs')
+const authentication = async () => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile : "credentials.json",
+    scopes : "https://www.googleapis.com/auth/spreadsheets"
+  })
 
-app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
-)
-app.use(express.static('public'))
+  const client = await auth.getClient()
 
-mongoose.connect('mongodb://localhost:27017/wikiDB', { useNewUrlParser: true })
-
-const articleSchema = {
-	title: String,
-	content: String,
+  const sheets = google.sheets({
+    version : 'v4',
+    auth: client
+  })
+  return {sheets}
 }
 
-const Article = mongoose.model('Article', articleSchema)
+//spreadsheetid
+const id = '1l73xbkg6QdtlgHeCYFEXDFAhV4cckgb_MH6dsmWrdqU'
 
-// Articles
-
-app
-	.route('/articles')
-
-	.get(function (req, res) {
-		Article.find(function (err, foundArticles) {
-			if (!err) {
-				res.send(foundArticles)
-			} else {
-				res.send(err)
-			}
-		})
-	})
-
-	.post(function (req, res) {
-		const newArticle = new Article({
-			title: req.body.title,
-			content: req.body.content,
-		})
-
-		newArticle.save(function (err) {
-			if (!err) {
-				res.send('Successfully added a new article.')
-			} else {
-				res.send(err)
-			}
-		})
-	})
-
-	.delete(function (req, res) {
-		Article.deleteMany(function (err) {
-			if (!err) {
-				res.send('Successfully deleted all articles.')
-			} else {
-				res.send(err)
-			}
-		})
-	})
-
-//A Specific Article
-
-app
-	.route('/articles/:articleTitle')
-
-	.get(function (req, res) {
-		Article.findOne({ title: req.params.articleTitle }, function (err, foundArticle) {
-			if (foundArticle) {
-				res.send(foundArticle)
-			} else {
-				res.send('No articles matching that title was found.')
-			}
-		})
-	})
-
-	.put(function (req, res) {
-		Article.update(
-			{ title: req.params.articleTitle },
-			{ title: req.body.title, content: req.body.content },
-			{ overwrite: true },
-			function (err) {
-				if (!err) {
-					res.send('Successfully updated the selected article.')
-				}
-			}
-		)
-	})
-
-	.patch(function (req, res) {
-		Article.update({ title: req.params.articleTitle }, { $set: req.body }, function (err) {
-			if (!err) {
-				res.send('Successfully updated article.')
-			} else {
-				res.send(err)
-			}
-		})
-	})
-
-	.delete(function (req, res) {
-		Article.deleteOne({ title: req.params.articleTitle }, function (err) {
-			if (!err) {
-				res.send('Successfully deleted the corresponding article.')
-			} else {
-				res.send(err)
-			}
-		})
-	})
-
-app.listen(3000, function () {
-	console.log('Server started on port 3000')
+//get route
+app.get('/', async(req,res) => {
+  try {
+    const { sheets } = await authentication();
+    //reading and storing
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId : id,
+      range : 'Sheet1',
+    })
+    res.send(response.data)
+  } catch (err) {
+    console.log(err)
+    res.status(500).send()
+  }
 })
+
+	const url =
+		'https://29fd8c842f4842d3752abbe76991fe74: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@x610s-existence.myshopify.com/admin/api/2022-01/products.json'
+  let productsData;
+	https.get(url, (response) => {
+		console.log(response.statusCode)
+		response.on('data', (data) => {
+			productsData = data.toString();
+      console.log(productsData)
+		})
+  })
+
+//post route
+app.post('/', async (req,res) => {
+  try {
+    //destructuring input values
+    const { orderId, orderName } =  req.body;
+    const { sheets } = await authentication()
+    //writing to spread sheet
+    const writeRequest = await sheets.spreadsheets.values.append({
+      spreadsheetId : id,
+      range : 'Sheet1',
+      valueInputOption : 'USER_ENTERED',
+      resource : {
+        values : [
+          [ orderId, orderName ],
+        ]
+      }
+    })
+    //validation
+    if(writeRequest.status === 200) {
+      return res.json({ msg : 'Spreadsheet updated successfully'})
+    }
+    return res.json({msg : 'Something went wrong while updating the spreadsheet'})
+  } catch(err) {
+    console.log('Error updating the spreadsheet',err)
+    res.status(500).send()
+  }
+})
+
+app.listen(3000, () => console.log('Server running on port 3000'))
